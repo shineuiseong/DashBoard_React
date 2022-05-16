13323667938bdf62ceb7ab935baa5270e446d687
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2022-05-16 23:16:04 +0900

    userList, New 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
+import { useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import Home from './pages/home/Home'
 import List from './pages/list/List'
 import Login from './pages/login/Login'
 import New from './pages/new/New'
 import Single from './pages/single/Single'
+import { DarkModeContext } from './context/darkModeContext'
+import './style/dark.scss'
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext)
   return (
-    <div className="App">
+    <div className={darkMode ? 'app dark' : 'app'}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,7 +5,8 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widget/Widget'
 import Featured from '../../components/featured/Featured'
-
+import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 const Home = () => {
   return (
     <div className="home">
@@ -20,8 +21,12 @@ const Home = () => {
         </div>
         <div className="charts">
           <Featured />
+          <Chart title="6개월 (수익)" aspect={2 / 1} />
         </div>
-        <div className="listContainer"></div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
       </div>
     </div>
   )
